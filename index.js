00d17bcb257bec22523a2879927dff9b01df453f
Barbara Tess <barbaratessn@gmail.com>
2024-01-18 14:22:27 +0300

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+const userId = document.getElementById('user-id')
+const errorMessage = document.getElementById('error-message')
+const submitData = (name, email) => {
+    const userData = { name, email }
+    const fetchConfigObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(userData),
+    }
+    return fetch('http://localhost:3000/users', fetchConfigObj)
+        .then((res) => res.json())
+        .then((data) => (userId.textContent = data.id))
+        .catch((error) => (errorMessage.textContent = error.message))
+}
